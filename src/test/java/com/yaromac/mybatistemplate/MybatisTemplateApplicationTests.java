7ea5cb9fbf3f81fbaa42b43900c23e6442f6a064
Yaroslav Darashkevich <f4stfurios@yandex.ru>
2023-02-25 13:34:41 +1300

    gitignore

---
+++ src/test/java/com/yaromac/mybatistemplate/MybatisTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaromac.mybatistemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
